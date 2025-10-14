af6db4d5c1a23f8c9374d434a128fd42e9687ecc
maxlipas <maxwolk567@gmail.com>
2025-10-14 11:58:33 +0300

    da

---
+++ #2/#2.cs
@@ -0,0 +1,20 @@
+﻿//Лабораторная работа #2. Разветвляющиеся алгоритмы
+//Условные операторы
+//высокий уровень Вариант #14
+try
+{
+    Console.Write("введите средний балл по диплому: ");
+    double a=double.Parse(Console.ReadLine());
+    Console.Write("введите стаж работы: ");
+    double b=double.Parse(Console.ReadLine());
+    if (b == 0) if (a * 1 >= 45) Console.WriteLine("прошел");
+    else Console.WriteLine("не прошел");
+    else if (b <=2 && b>=0) if (a * 13 >= 45) Console.WriteLine("прошел");
+    else Console.WriteLine("не прошел");
+    else if (b>=2 && b <= 5) if (a * 16 >= 45) Console.WriteLine("прошел");
+    else Console.WriteLine("не прошел");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ #3/#3.cs
@@ -0,0 +1,24 @@
+﻿//Лабораторная работа #2. Разветвляющиеся алгоритмы
+//Оператор выбора
+//Средний уровень Вариант #14
+try
+{
+    Console.Write("введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    double a = 0.5;
+    double b = 4.5;
+    double z = Math.Pow(Math.E, a * x);
+    if (x <= 5 * a) Console.WriteLine($"y={2.5 * Math.Pow(b, 2) + a * x - 4.5 * Math.Cos(x * z)}");
+    else a = 0.5;
+    b = 3.7;
+    z = Math.Pow(Math.E, 2 * a * x);
+    if (x > b) Console.WriteLine($"y={Math.Pow(a * a - 5.4 * x, 3) + Math.Log10(x * z)}");
+    else a = 0.5;
+    b = 2.7;
+    z = Math.Pow(Math.E, 2.5 * a * x);
+    if (x > 5 * a && x <= b) Console.WriteLine($"y={Math.Sqrt(6.5 * (b * b) + (a - Math.Pow(x, 3) * z))}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
