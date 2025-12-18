39bfa6326d1d7be45a90de1c55c912ff1f84e55d
maxlipas <maxlipskiy2009@gmail.com>
2025-12-18 14:33:02 +0300

    sw

--- #3/#3.cs
+++ #3/#3.cs
@@ -3,20 +3,49 @@
 //Средний уровень Вариант #14
 try
 {
-    Console.Write("введите x: ");
+    Console.Write("Введите x: ");
     double x = double.Parse(Console.ReadLine());
-    double a = 0.5;
-    double b = 4.5;
-    double z = Math.Pow(Math.E, a * x);
-    if (x <= 5 * a) Console.WriteLine($"y={2.5 * Math.Pow(b, 2) + a * x - 4.5 * Math.Cos(x * z)}");
-    else a = 0.5;
-    b = 3.7;
-    z = Math.Pow(Math.E, 2 * a * x);
-    if (x > b) Console.WriteLine($"y={Math.Pow(a * a - 5.4 * x, 3) + Math.Log10(x * z)}");
-    else a = 0.5;
-    b = 2.7;
-    z = Math.Pow(Math.E, 2.5 * a * x);
-    if (x > 5 * a && x <= b) Console.WriteLine($"y={Math.Sqrt(6.5 * (b * b) + (a - Math.Pow(x, 3) * z))}");
+    double y = 0;
+    double a, b, z;
+    int condition=0;
+    if (x <= 5 * 0.5)
+    {
+        condition = 1;
+    }
+    else if (x > 3.7) 
+    {
+        condition = 2;
+    }
+    else if(5*0.5<x||x<=2.7)
+    {
+        condition = 3;
+    }
+    switch (condition)
+    {
+        case 1:
+            a = 0.5;
+            b = 4.5;
+            z = Math.Pow(Math.E, a * x);
+            y = 2.5 * Math.Pow(b, 2) + a * x - 4.5 * Math.Cos(x * z);
+            Console.WriteLine($"y = {y}");
+            break;
+
+        case 2: 
+            a = 0.5;
+            b = 3.7;
+            z = Math.Pow(Math.E, 2 * a * x);
+            y = Math.Pow(a * a - 5.4 * x, 3) + Math.Log10(x * z);
+            Console.WriteLine($"y = {y}");
+            break;
+
+        case 3: 
+            a = 0.5;
+            b = 2.7;
+            z = Math.Pow(Math.E, 2.5 * a * x);
+            y = Math.Sqrt(6.5 * Math.Pow(b, 2) + (a - Math.Pow(x, 3) * z));
+            Console.WriteLine($"y = {y}");
+            break;
+    }
 }
 catch(Exception e)
 {
--- #3/obj/Debug/net9.0/#3.AssemblyInfo.cs
+++ #3/obj/Debug/net9.0/#3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("#3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6db4d5c1a23f8c9374d434a128fd42e9687ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("#3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("#3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
